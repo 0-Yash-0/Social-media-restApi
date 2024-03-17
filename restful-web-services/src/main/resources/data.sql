7b883f508ccb6dff86eed10af7a7a8d226f7fba5
Yasht <Yasht@host.docker.internal>
2024-03-17 15:46:35 +0530

    social-media-restapi

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+insert into user_details(id,birth_date,name)
+values(10001, current_date(), 'Yash');
+
+insert into user_details(id,birth_date,name)
+values(10002, current_date(), 'Annanya');
+
+insert into user_details(id,birth_date,name)
+values(10003, current_date(), 'Mayank');
+
+insert into post(id,description,user_id)
+values(20001,'I want to learn Java', 10001);
+
+insert into post(id,description,user_id)
+values(20002,'I want to learn Cpp', 10001);
+
+insert into post(id,description,user_id)
+values(20003,'I want to Get AWS Certified', 10002);
+
+insert into post(id,description,user_id)
+values(20004,'I want to learn Cloud Computing', 10002);
